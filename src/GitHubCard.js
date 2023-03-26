a86f0409b219705e78f61501900f4f7ecf43a733
INS140 <IsaacNSaettel@gmail.com>
2023-03-25 23:03:27 -0700

    finished snapshot testing

---
+++ src/GitHubCard.js
@@ -0,0 +1,9 @@
+import { Card } from "react-bootstrap";
+
+export default function GitHubCard() {
+    return <Card>
+        <h1>INS140</h1>
+        <img src='./githubProfilePhoto.png' alt='github pprofile photo'/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </Card>
+}
